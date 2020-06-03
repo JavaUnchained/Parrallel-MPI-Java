afcd61652cbb3393d1098517528430acfea7f79b
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-04 02:01:05 +0300

    fix generator and structured project

--- src/Serial.java
+++ src/Serial.java
@@ -7,7 +7,7 @@ public class Serial {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
-        int[] shuf = new ListGenerator(40_000_000).getInts();
+        int[] shuf = new ListGenerator(20_000_000).getInts();
         int[] sort = MergeSort.sortArray(shuf);
         System.out.printf("Shuffled: %s \n",Arrays.toString(shuf));
         System.out.printf("Sorted  : %s \n", Arrays.toString(sort));
