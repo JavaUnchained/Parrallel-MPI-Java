542b08d55f6b561c4772d5f534ae91cd3696fffa
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-05 00:08:06 +0300

    non

--- src/Serial.java
+++ src/Serial.java
@@ -7,7 +7,7 @@ public class Serial {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
-        int[] shuf = new ListGenerator(20_000_000).getInts();
+        int[] shuf = new ListGenerator(58_000_000).getInts();
         int[] sort = MergeSort.sortArray(shuf);
         System.out.printf("Shuffled: %s \n",Arrays.toString(shuf));
         System.out.printf("Sorted  : %s \n", Arrays.toString(sort));
